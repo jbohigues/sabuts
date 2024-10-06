07c8c02f630561460b495528e997de82f6f3510b
Juan Bohigues <jbohigues@vidal-ac.es>
2024-10-06 12:43:44 +0200

    feat: :lipstick: edit som styles and comment unnecessary code

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,12 +14,9 @@ import {
   IonItem,
 } from '@ionic/angular/standalone';
 
-import { CreateUserDto } from 'src/app/shared/dto/user-dto';
-import { Colors } from 'src/app/shared/enums/colors';
-import { IconsToast } from 'src/app/shared/enums/iconsToast';
-import { UserModel } from 'src/app/shared/models/users.models';
-import { FirestoreService } from 'src/app/shared/services/firestore.service';
-import { ValidatorService } from 'src/app/shared/services/validator.service';
+// import { CreateUserModelDto, UserModel } from 'src/app/models/users.model';
+// import { FirestoreService } from 'src/app/services/firestore.service';
+// import { ValidatorService } from 'src/app/shared/services/validator.service';
 
 @Component({
   selector: 'app-home',
@@ -42,80 +39,74 @@ import { ValidatorService } from 'src/app/shared/services/validator.service';
   ],
 })
 export class HomePage {
-  users: UserModel[] = [];
-
-  constructor(
-    private firestoreService: FirestoreService,
-    private validatorService: ValidatorService
-  ) {
-    this.loadUsers();
-  }
-
-  loadUsers() {
-    this.firestoreService.getCollectionChanges<UserModel>('users').subscribe({
-      next: (res) => {
-        if (res) this.users = res;
-      },
-      error: (e) => {
-        console.error(e);
-      },
-    });
-  }
-
-  async createUser() {
-    let user: CreateUserDto = {
-      email: 'prueba',
-      password: '1234',
-      firstName: 'prueba',
-      lastName: 'prueba',
-      userName: 'prueba',
-      avatarId: '',
-      totalPoints: 10,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      isAdmin: false,
-      active: true,
-    };
-
-    try {
-      // Verificar si el email ya existe
-      const emailExists = await this.validatorService.checkIfEmailExists(
-        user.email
-      );
-      if (emailExists) {
-        await this.validatorService.presentToast(
-          'Ja existeix un usuari amb aquest correu electrònic',
-          Colors.danger,
-          IconsToast.danger_close_circle
-        );
-        return;
-      }
-
-      // Verificar si el username ya existe
-      const usernameExists = await this.validatorService.checkIfUsernameExists(
-        user.userName
-      );
-      if (usernameExists) {
-        await this.validatorService.presentToast(
-          "Ja existeix un usuari amb aquest nom d'usuari",
-          Colors.danger,
-          IconsToast.danger_trash
-        );
-        return;
-      }
-
-      // Si no existe ni el email ni el username, procedemos a crear el usuario
-      const result = await this.firestoreService.createDocument(user, 'users');
-      if (result) {
-        await this.validatorService.presentToast(
-          'Usuari creat exitosament',
-          Colors.success,
-          IconsToast.success_checkmark_circle
-        );
-        return;
-      }
-    } catch (error) {
-      console.error('Error al crear el usuari:', error);
-    }
-  }
+  // users: UserModel[] = [];
+  // constructor(
+  //   private firestoreService: FirestoreService,
+  //   private validatorService: ValidatorService
+  // ) {
+  //   this.loadUsers();
+  // }
+  // loadUsers() {
+  //   this.firestoreService.getCollectionChanges<UserModel>('users').subscribe({
+  //     next: (res) => {
+  //       if (res) this.users = res;
+  //     },
+  //     error: (e) => {
+  //       console.error(e);
+  //     },
+  //   });
+  // }
+  // async createUser() {
+  //   let user: CreateUserModelDto = {
+  //     email: 'prueba',
+  //     password: '1234',
+  //     firstName: 'prueba',
+  //     lastName: 'prueba',
+  //     userName: 'prueba',
+  //     avatarId: '',
+  //     totalPoints: 10,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //     isAdmin: false,
+  //     active: true,
+  //   };
+  //   try {
+  //     // Verificar si el email ya existe
+  //     const emailExists = await this.validatorService.checkIfEmailExists(
+  //       user.email
+  //     );
+  //     if (emailExists) {
+  //       await this.validatorService.presentToast(
+  //         'Ja existeix un usuari amb aquest correu electrònic',
+  //         Colors.danger,
+  //         IconsToast.danger_close_circle
+  //       );
+  //       return;
+  //     }
+  //     // Verificar si el username ya existe
+  //     const usernameExists = await this.validatorService.checkIfUsernameExists(
+  //       user.userName
+  //     );
+  //     if (usernameExists) {
+  //       // await this.validatorService.presentToast(
+  //         "Ja existeix un usuari amb aquest nom d'usuari",
+  //         Colors.danger,
+  //         IconsToast.danger_trash
+  //       );
+  //       return;
+  //     }
+  //     // Si no existe ni el email ni el username, procedemos a crear el usuario
+  //     const result = await this.firestoreService.createDocument(user, 'users');
+  //     if (result) {
+  //       await this.validatorService.presentToast(
+  //         'Usuari creat exitosament',
+  //         Colors.success,
+  //         IconsToast.success_checkmark_circle
+  //       );
+  //       return;
+  //     }
+  //   } catch (error) {
+  //     console.error('Error al crear el usuari:', error);
+  //   }
+  // }
 }
