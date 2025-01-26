184c596d5957cc62c2d60b093578d9575ff402da
jbohigues <jbohigues.depweb@gmail.com>
2025-01-26 09:49:55 +0100

    feat: :hammer: add android of capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sabuts',
+  appName: 'Sabuts',
   webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
