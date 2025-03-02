f2a2f60b9dce504b9715f6408adc11f8dc8db3e6
jbohigues <jbohigues.depweb@gmail.com>
2025-03-02 11:12:44 +0100

    feat: :hammer: change command script in package.json

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  maxCorrectAnswers: 3,
+  pointsToWinGame: 15,
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+};
